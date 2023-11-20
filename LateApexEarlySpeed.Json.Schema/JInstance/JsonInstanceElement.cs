7e376646de4ba85d239dfa3bcebe9ae5599d3620
lateapexearlyspeed <lateapexearlyspeed@163.com>
2023-11-20 13:23:19 +0800

    JsonInstanceElement implements Equals().

--- LateApexEarlySpeed.Json.Schema/JInstance/JsonInstanceElement.cs
+++ LateApexEarlySpeed.Json.Schema/JInstance/JsonInstanceElement.cs
@@ -3,7 +3,7 @@ using LateApexEarlySpeed.Json.Schema.Common;
 
 namespace LateApexEarlySpeed.Json.Schema.JInstance;
 
-public readonly struct JsonInstanceElement
+public readonly struct JsonInstanceElement : IEquatable<JsonInstanceElement>
 {
     private readonly JsonElement _jsonElement;
     private readonly ImmutableJsonPointer _instanceLocation;
@@ -44,5 +44,57 @@ public readonly struct JsonInstanceElement
     public string GetRawText()
     {
         return _jsonElement.GetRawText();
+    }
+
+    public bool Equals(JsonInstanceElement other)
+    {
+        if (ValueKind != other.ValueKind)
+        {
+            return false;
+        }
+
+        switch (ValueKind)
+        {
+            case JsonValueKind.True:
+            case JsonValueKind.False:
+            case JsonValueKind.Null:
+                return true;
+
+            case JsonValueKind.String:
+                return GetString() == other.GetString();
+
+            case JsonValueKind.Number:
+                return GetRawText() == other.GetRawText();
+
+            case JsonValueKind.Array:
+                return EnumerateArray().SequenceEqual(other.EnumerateArray());
+
+            case JsonValueKind.Object:
+                Dictionary<string, JsonInstanceElement> otherProperties = other.EnumerateObject().ToDictionary(prop => prop.Name, prop => prop.Value);
+                return EnumerateObject().Count() == otherProperties.Count
+                       && EnumerateObject().All(prop => otherProperties.TryGetValue(prop.Name, out JsonInstanceElement otherValue) && otherValue.Equals(prop.Value));
+            default:
+                return false;
+        }
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is JsonInstanceElement other && Equals(other);
+    }
+
+    public static bool operator ==(JsonInstanceElement left, JsonInstanceElement right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(JsonInstanceElement left, JsonInstanceElement right)
+    {
+        return !left.Equals(right);
+    }
+
+    public override int GetHashCode()
+    {
+        throw new NotImplementedException();
     }
 }
