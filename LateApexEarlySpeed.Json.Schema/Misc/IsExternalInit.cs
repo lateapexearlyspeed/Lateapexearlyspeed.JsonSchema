9ccbcb28590b8a757a726851b2a7a1f80cc20430
lateapexearlyspeed <lateapexearlyspeed@163.com>
2024-09-06 17:48:49 +0800

    Remove unnecessary IsExternalInit file when is on .net 6.

--- LateApexEarlySpeed.Json.Schema/Misc/IsExternalInit.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.ComponentModel;
-
-// This file is to support 'init' setter feature which is only available from .NET 5.0, refer: https://www.mking.net/blog/error-cs0518-isexternalinit-not-defined
-namespace System.Runtime.CompilerServices;
-
-[EditorBrowsable(EditorBrowsableState.Never)]
-internal static class IsExternalInit { }
