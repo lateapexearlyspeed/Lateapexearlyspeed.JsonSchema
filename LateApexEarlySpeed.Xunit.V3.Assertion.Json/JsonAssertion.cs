d84a57ab996fdaeb31c3d9a0542637a7b560ee08
lateapexearlyspeed <lateapexearlyspeed@163.com>
2025-02-08 16:37:54 +0800

    Create LateApexEarlySpeed.Xunit.V3.Assertion.Json project from existing LateApexEarlySpeed.Xunit.Assertion.Json project.

---
+++ LateApexEarlySpeed.Xunit.V3.Assertion.Json/JsonAssertion.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using LateApexEarlySpeed.Json.Schema;
+using LateApexEarlySpeed.Json.Schema.Common;
+using LateApexEarlySpeed.Json.Schema.FluentGenerator;
+using Xunit.Sdk;
+
+namespace LateApexEarlySpeed.Xunit.V3.Assertion.Json
+{
+    /// <summary>
+    /// Class to make assertion about json data
+    /// </summary>
+    public static class JsonAssertion
+    {
+        /// <summary>
+        /// Assert that <paramref name="actualJson"/> should meet requirement specified from <paramref name="expectedSchemaConfiguration"/>
+        /// </summary>
+        /// <param name="expectedSchemaConfiguration">Json data requirement</param>
+        /// <param name="actualJson">actual json text</param>
+        /// <exception cref="JsonAssertException">If assertion fails, will throw and report error reason and failed json location</exception>
+        public static void Meet(Action<JsonSchemaBuilder> expectedSchemaConfiguration, string actualJson)
+        {
+            var jsonSchemaBuilder = new JsonSchemaBuilder();
+            expectedSchemaConfiguration(jsonSchemaBuilder);
+            JsonValidator jsonValidator = jsonSchemaBuilder.BuildValidator();
+            ValidationResult validationResult = jsonValidator.Validate(actualJson);
+
+            if (!validationResult.IsValid)
+            {
+                throw new JsonAssertException($"{nameof(JsonAssertion)}.{nameof(Meet)}() Failure: {validationResult.ErrorMessage}, location (in json pointer format): \"{validationResult.InstanceLocation}\"");
+            }
+        }
+
+        /// <summary>
+        /// Assert that <paramref name="actualJson"/> should be json-level equivalent to <paramref name="expectedJson"/>
+        /// </summary>
+        /// <param name="expectedJson">Expected json structure</param>
+        /// <param name="actualJson">Actual json data</param>
+        /// <exception cref="JsonAssertException">If assertion fails, will throw and report error reason and failed json location</exception>
+        public static void Equivalent(string expectedJson, string actualJson)
+        {
+            var jsonSchemaBuilder = new JsonSchemaBuilder();
+            jsonSchemaBuilder.Equivalent(expectedJson);
+            JsonValidator jsonValidator = jsonSchemaBuilder.BuildValidator();
+            ValidationResult validationResult = jsonValidator.Validate(actualJson);
+
+            if (!validationResult.IsValid)
+            {
+                throw new JsonAssertException($"{nameof(JsonAssertion)}.{nameof(Equivalent)}() Failure: {validationResult.ErrorMessage}, location (in json pointer format): \"{validationResult.InstanceLocation}\"");
+            }
+        }
+    }
+
+    public class JsonAssertException : XunitException
+    {
+        public JsonAssertException(string? userMessage) : base(userMessage)
+        {
+        }
+    }
+}
---
+++ LateApexEarlySpeed.Xunit.V3.Assertion.Json/nuget.readme.md
@@ -0,0 +1,86 @@
+﻿Provide fluent json assertion for Xunit v3.
+
+There were already json related test assertion libraries, most ones were asserting json's equivalent.
+
+However, imagine what you would test is other factors besides of equivalent on whole json, remember the assertion methods in Xunit (Contains, AllOf, IsTypeOf ..) at ANY json node location.
+
+This library can assert json node data in any node location, not only against whole json. Also, you won't write multiple assertion code lines for different json node, you just need to have one JsonAssertion.Meet(...) method then inside it, you specify what you would test, all in one place. When assertion failed, library will throw detailed assert exception which reports failed json node location. 
+
+```csharp
+JsonAssertion.Meet(b =>
+                        b.IsJsonObject()
+                            .HasProperty("p1", b => b.IsJsonObject())
+                            .HasProperty("p2", b => b.IsJsonNumber().IsLessThan(5)),
+                """
+                {
+                  "p1": {},
+                  "p2": 5.1
+                }
+                """);
+```
+
+```
+LateApexEarlySpeed.Xunit.Assertion.Json.JsonAssertException
+JsonAssertion.Meet() Failure: Instance '5.1' is equal to or greater than '5', location (in json pointer format): '/p2'
+```
+
+Also it supports json-equivalent assertion:
+
+```csharp
+JsonAssertion.Equivalent("""
+                {
+                  "a": 1,
+                  "b": 2
+                }
+                """,
+                """
+                {
+                  "b": 2,
+                  "a": 1
+                }
+                """);
+```
+
+Available assertion methods for json:
+
+- NotJsonNull
+- IsJsonTrue
+- IsJsonFalse
+- IsJsonBoolean
+- IsJsonNull
+- IsJsonString:
+  - Equal
+  - IsIn
+  - HasMaxLength
+  - HasMinLength
+  - HasPattern
+  - HasCustomValidation
+- IsJsonNumber:
+  - Equal
+  - IsIn
+  - IsGreaterThan
+  - IsLessThan
+  - NotGreaterThan
+  - NotLessThan
+  - MultipleOf
+  - HasCustomValidation
+- IsJsonArray:
+  - SerializationEquivalent
+  - HasItems
+  - HasLength
+  - HasMaxLength
+  - HasMinLength
+  - HasUniqueItems
+  - HasCustomValidation
+  - Contains
+  - NotContains
+  - Equivalent
+- IsJsonObject:
+  - SerializationEquivalent
+  - HasProperty
+  - HasCustomValidation
+  - Equivalent
+  - HasNoProperty
+- Or
+
+There are HasCustomValidation() overloads which can be used to create more custom validation logic unit.
