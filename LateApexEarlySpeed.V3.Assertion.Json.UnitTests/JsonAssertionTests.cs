1a2af84f8689c83078f0cdb3b17a0f2fb34b18c1
lateapexearlyspeed <lateapexearlyspeed@163.com>
2025-02-11 16:01:01 +0800

    Add LateApexEarlySpeed.V3.Assertion.Json.UnitTests project.

---
+++ LateApexEarlySpeed.V3.Assertion.Json.UnitTests/JsonAssertionTests.cs
@@ -0,0 +1,81 @@
+using LateApexEarlySpeed.Xunit.V3.Assertion.Json;
+
+namespace LateApexEarlySpeed.V3.Assertion.Json.UnitTests
+{
+    public class JsonAssertionTests
+    {
+        [Fact]
+        public void Meet_ValidData()
+        {
+            JsonAssertion.Meet(b => 
+                b.IsJsonObject()
+                .HasProperty("p1")
+                .HasProperty("p2", b => b.IsJsonNumber().Equal(5)), 
+                """
+                {
+                  "p1": null,
+                  "p2": 5
+                }
+                """);
+        }
+
+        [Fact]
+        public void Meet_InvalidData_Throw()
+        {
+            JsonAssertException jsonAssertException = Assert.Throws<JsonAssertException>(() =>
+            {
+                JsonAssertion.Meet(b =>
+                        b.IsJsonObject()
+                            .HasProperty("p1")
+                            .HasProperty("p2", b => b.IsJsonNumber().Equal(5)),
+                    """
+                {
+                  "p1": null,
+                  "p2": 4.9
+                }
+                """);
+            });
+
+            Assert.Equal("JsonAssertion.Meet() Failure: Number not same, one is '5' but another is '4.9', location (in json pointer format): \"/p2\"", jsonAssertException.Message);
+        }
+
+        [Fact]
+        public void Equivalent_ValidData()
+        {
+            JsonAssertion.Equivalent("""
+                {
+                  "a": 1,
+                  "b": 2
+                }
+                """,
+                """
+                {
+                  "b": 2,
+                  "a": 1
+                }
+                """);
+        }
+
+        [Fact]
+        public void Equivalent_InvalidData_Throw()
+        {
+            JsonAssertException jsonAssertException = Assert.Throws<JsonAssertException>(() =>
+            {
+                JsonAssertion.Equivalent("""
+                {
+                  "a": 1,
+                  "b": 2
+                }
+                """,
+                    """
+                {
+                  "a": 1,
+                  "b": 3
+                }
+                """);
+            });
+
+            Assert.Equal("JsonAssertion.Equivalent() Failure: Number not same, one is '2' but another is '3', location (in json pointer format): \"/b\"", jsonAssertException.Message);
+        }
+    }
+}
