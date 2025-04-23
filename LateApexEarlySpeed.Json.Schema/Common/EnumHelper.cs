275eef5b4e0551f13fec331936576ec574071740
lateapexearlyspeed <lateapexearlyspeed@163.com>
2025-04-23 11:05:07 +0800

    Cache enum names (for slow Enum.ToString() which is low cpu perf impact.)

---
+++ LateApexEarlySpeed.Json.Schema/Common/EnumHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Concurrent;
+
+namespace LateApexEarlySpeed.Json.Schema.Common;
+
+internal static class EnumHelper<TEnum> where TEnum : Enum
+{
+    private static readonly ConcurrentDictionary<TEnum, string> EnumNamesCache = new(Environment.ProcessorCount, Enum.GetValues(typeof(TEnum)).Length);
+
+    public static string GetCachedStringName(TEnum enumValue)
+    {
+        return EnumNamesCache.GetOrAdd(enumValue, enumArg => enumArg.ToString());
+    }
+}
--- LateApexEarlySpeed.Json.Schema/Keywords/TypeKeyword.cs
+++ LateApexEarlySpeed.Json.Schema/Keywords/TypeKeyword.cs
@@ -44,7 +44,7 @@ internal class TypeKeyword : KeywordBase
 
     private string GetErrorMessage(JsonValueKind actualKind)
     {
-        return $"Expect type(s): '{string.Join('|', InstanceTypes)}' but actual is '{actualKind}'";
+        return $"Expect type(s): '{string.Join('|', InstanceTypes.Select(EnumHelper<InstanceType>.GetCachedStringName))}' but actual is '{EnumHelper<JsonValueKind>.GetCachedStringName(actualKind)}'";
     }
 
     private bool IsValidAgainstType(JsonInstanceElement instance, InstanceType expectedInstanceType)
